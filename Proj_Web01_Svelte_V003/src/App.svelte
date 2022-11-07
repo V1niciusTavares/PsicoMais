87624ada880fb340cd95ca55dadfd3536931d950
ijbs-dev <smaeljbs@gmail.com>
2022-11-07 19:58:47 -0300

    Inserindo Projeto Funcional em SVELTE
    Foi adicionado nova pasta com projeto em sua 3 versão. Esta funcionando com tela de Login e cadastro Usuario.

---
+++ Proj_Web01_Svelte_V003/src/App.svelte
@@ -0,0 +1,32 @@
+<script>
+  import Register from "./Register.svelte";
+  import Login from "./Login.svelte";
+  import Home from "./Home.svelte";
+  import { page } from "./stores";
+
+  let currentPage;
+
+  page.subscribe((value) => {
+    currentPage = value;
+  });
+
+  function changePage(p) {
+    currentPage = p;
+  }
+</script>
+
+<main>
+ <!--<h1>Testando Login ({currentPage})</h1>-->
+
+  {#if currentPage == "home"}
+    <Home />
+    {:else if currentPage == "loginUsuario" || currentPage == "longinProf" || currentPage == "loginInst"}
+      <Login />
+    {:else if currentPage == "registerUsuario" || currentPage == "registerProf" || currentPage == "registerInst"}
+      <Register />
+    {/if}  
+  
+</main>
+
+<style>
+</style>
