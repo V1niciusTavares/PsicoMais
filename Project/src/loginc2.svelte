81fda41315316487ac83884da431ff2ffb91c02c
tiagopessoa35320 <tcpf@discente.ifpe.edu.br>
2022-12-23 01:07:10 -0300

    login de tiago

---
+++ Project/src/loginc2.svelte
@@ -0,0 +1,27 @@
+<head>
+    <meta charset="UTF-8">
+   <link rel="stylesheet" href="css/..css"
+   
+</head>
+<body>
+    <section class="area login">
+        <div class="login">
+                    
+         <div>
+          
+            <img src="psico.png">
+        </div>
+
+        <form method="post" >
+            <input type="text" name="nome" placeholder="nome do usuario" autofocus>
+            <input type="password" name="senha" placeholder="sua senha">
+            <input type="submit" value="entrar">
+
+         </form>
+         <p>ainda nao tem uma conta?<a href="#">criar conta</a></p>
+       
+         
+       </div>
+
+    </section>
+</body>
